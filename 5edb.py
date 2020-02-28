2eee36b9faae2994294062f9aa46afe7e0531630
Ted Tibbetts <ted@circulated.ca>
2020-02-28 12:02:28 +0100

    Refactored code into functions.

--- 5edb.py
+++ 5edb.py
@@ -4,35 +4,42 @@ from collections import defaultdict
 
 debug = print
 
-debug('Parsing xml...')
-parser = etree.XMLParser()
-with open('FC5eXML/CoreOnly.xml', 'r') as xmlfile:
-    tree = etree.parse(xmlfile, parser)
-debug('...done')
-
-spells = tree.xpath("//spell")
-spell_nodes = tree.xpath("//spell/*")
-print(len(spells))
-print(len(spell_nodes))
-print(dir(spell_nodes[0]))
-print(spell_nodes[0].__class__)
-print(spell_nodes[0].tag)
-
-spell_tags = set((node.tag for node in spell_nodes))
-print(spell_tags)
-
 def group(iterable, key):
     ret = defaultdict(list)
     for i in iterable:
         ret[key(i)].append(i)
     return ret
 
-spell_tag_groups = group(spell_nodes, lambda n: n.tag).items()
-for k, g in spell_tag_groups:
-    print("{0}: {1} nodes".format(k, len(g)))
-    value_group = group(g, lambda n: n.text)
-    if len(value_group.keys()) > 20:
-        print("  {0} unique values".format(len(value_group.keys())))
-    else:
-        for h, i in value_group.items():
-            print("  {0}: {1} nodes".format(h, len(i)))
+def parse_db(db_file='FC5eXML/CoreOnly.xml'):
+    debug('Parsing xml...')
+    parser = etree.XMLParser()
+    with open(db_file, 'r') as xmlfile:
+        tree = etree.parse(xmlfile, parser)
+    debug('...done')
+    return tree
+
+def spell_tag_analysis(tree):
+    spells = tree.xpath("//spell")
+    spell_nodes = tree.xpath("//spell/*")
+    print(len(spells))
+    print(len(spell_nodes))
+    print(dir(spell_nodes[0]))
+    print(spell_nodes[0].__class__)
+    print(spell_nodes[0].tag)
+
+    spell_tags = set((node.tag for node in spell_nodes))
+    print(spell_tags)
+
+    spell_tag_groups = group(spell_nodes, lambda n: n.tag).items()
+    for k, g in spell_tag_groups:
+        print("{0}: {1} nodes".format(k, len(g)))
+        value_group = group(g, lambda n: n.text)
+        if len(value_group.keys()) > 20:
+            print("  {0} unique values".format(len(value_group.keys())))
+        else:
+            for h, i in value_group.items():
+                print("  {0}: {1} nodes".format(h, len(i)))
+
+if __name__ == '__main__':
+    tree = parse_db()
+    spell_tag_analysis(tree)
