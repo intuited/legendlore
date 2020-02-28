8d96c27f43c6b1b89b5c86072508cc7574a45d30
Ted Tibbetts <ted@circulated.ca>
2020-02-28 19:24:34 +0100

    Transformed xml tree into object hierarchy.

--- 5edb.py
+++ 5edb.py
@@ -1,6 +1,8 @@
 from lxml import etree
 from itertools import groupby
 from collections import defaultdict
+import re
+from pprint import pprint
 
 debug = print
 
@@ -46,6 +48,135 @@ def spell_tag_analysis(tree):
             for h, i in sort_group(value_group):
                 print("  {0}: {1} nodes".format(h, len(i)))
 
+def parse_casting_time(time):
+    #TODO: write this, validate
+    # Why are there None values for this?
+    return time
+def parse_spell_range(r):
+    #TODO: write this, validate
+    return r
+
+def parse_spell_components(components):
+    """Returns a dictionary with form resembling
+
+    {'V': True,
+     'S': False,
+     'M': "a sprig of rosemary"}
+
+    Initial strings are comma-separated strings of one of these forms:
+    * V
+    * S
+    * M (...)
+    """
+    if components is None:
+        return []
+    c = []
+    m = re.match('^[^(,]*,', components.strip())
+    while m:
+        c.append(m.group())
+        components = components[m.end():]
+        m = re.match('^[^(,]*,', components.strip())
+    return c
+
+    """ uhhhh
+    ret = {}
+    m = re.match('^\s*(V|S|M\s*\([^)]*\)(,\s*)*)+\s*$')
+
+    for c in components:
+        if c[0] == 'V':
+            ret['V'] = True
+            assert len(c) == 1
+        elif c[0] == 'S'
+            ret['S'] = True
+            assert len(c) == 1
+        elif c[0] == 'M'
+    """
+    #TODO: finish this
+    return components
+
+def parse_spell_duration(duration):
+    """Return: concentration, duration = ({True, False}, [STRING])"""
+    #TODO: add validation
+    if duration is None:
+        return False, None
+
+    if duration[:15] == 'Concentration, ':
+        return True, duration[15:]
+    else:
+        return False, duration
+
+def parse_spell_classes(classes):
+    if classes is None:
+        return []
+    classes = re.split(',\s*', classes)
+    classes = [c.strip() for c in classes]
+    """I thiiiiink I don't need this
+    def parse_subclass(c):
+        m = re.match('^\s*(?P<class>[^(]]+)\s*'
+                     + '(?P<subclass>\([^)]+\))?\s*$', c)
+        m = m.groups()
+        c = m[0]
+        s = m[1][1:][:-1].trim().rtrim()
+        return c, s
+    """
+    return sorted(classes)
+
+def parse_spell_text(lines):
+    state = {'source': None}
+    def process(lines):
+        for line in lines:
+            if line is None:
+                yield ''
+            elif line[:8] == 'Source: ':
+                if state['source']:
+                    debug('Found two "Source: " lines')
+                state['source'] = line[8:]
+            else:
+                yield line.strip()
+    text = '\n'.join(process(lines))
+    return text, state['source']
+
+"""thing to parse the initial db.
+
+okay so it needs to verify that it's correctly parsed things
+by generating a 
+"""
+
+def parse_spells(tree):
+    spells = tree.xpath("//spell")
+    schools = {'EV': "Evocation",
+               'T': "Transmutation",
+               'C': "Conjuration",
+               'A': "Abjuration",
+               'EN': "Enchantment",
+               'D': "Divination",
+               'N': "Necromancy",
+               'I': "Illusion",
+               None: None}
+    for node in spells:
+        spell = {}
+        spell['name'] = node.find('name').text
+        spell['level'] = int(node.find('level').text)
+        #TODO: validation to confirm that this value is between 1 and 9
+        spell['school'] = schools[getattr(node.find('school'), 'text', None)]
+        spell['ritual'] = True if node.find('ritual') == "YES" else False
+        spell['time'] = parse_casting_time(node.find('time').text)
+        spell['range'] = parse_spell_range(node.find('range').text)
+        spell['components'] = parse_spell_components(node.find('components').text)
+        spell['concentration'], spell['duration'] = parse_spell_duration(node.find('duration').text)
+        spell['classes'] = parse_spell_classes(node.find('classes').text)
+        spell['text'] = parse_spell_text(n.text for n in node.findall('text'))
+        spell['roll'] = getattr(node.find('roll'), 'text', None)
+        #TODO: figure out what to do with this property
+        yield spell
+
+def parsed_spells_analysis(spells):
+    print('spell count: {0}'.format(len(spells)))
+    print('first spell:')
+    pprint(spells[0])
+
 if __name__ == '__main__':
     tree = parse_db()
-    spell_tag_analysis(tree)
+    #spell_tag_analysis(tree)
+    parsed = list(parse_spells(tree))
+    parsed_spells_analysis(parsed)
