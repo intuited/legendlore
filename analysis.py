4481cd02fe1545d475bf82274eef32b91b9d2813
Ted Tibbetts <ted@circulated.ca>
2020-10-08 22:49:36 -0300

    Added print_spell_list() utility function.

--- analysis.py
+++ analysis.py
@@ -192,3 +192,27 @@ def battle_royale_creatures():
 
     pare = lambda mlist: [m for m in mlist if m.cr % 2 == 1 and m.cr > 1 and m.cr < 19]
     printms = lambda mlist: '\n'.join(f'* {int(m.cr)}: {m.name}' for m in mlist)
+
+# Some utility functions that may be useful in the future
+def print_spell_list(spells):
+    """Prints formatted spell info for each spell in the list.
+
+    Format:
+        * SpellName Summary
+          " Spell Text Line 1
+          " Spell Text Line 2
+          " ...
+
+    The "SpellName Summary" line is the output of Spell.oneline().
+
+    >>> print_spell_list(dnd5edb.Spells().where(name='Compulsion'))
+    * Compulsion A/30'/C<=1m (4:B)
+      " Creatures of your choice that you can see within range and that can hear you must make a Wisdom saving throw. A target automatically succeeds on this saving throw if it can't be charmed. On a failed save, a target is affected by this spell. Until the spell ends, you can use a bonus action on each of your turns to designate a direction that is horizontal to you. Each affected target must use as much of its movement as possible to move in that direction on its next turn. It can take its action before it moves. After moving in this way, it can make another Wisdom saving throw to try to end the effect.
+      " A target isn't compelled to move into an obviously deadly hazard, such as a fire pit, but it will provoke opportunity attacks to move in the designated direction.
+    """
+    def indent(text):
+        return '\n'.join('  " ' + line for line in text.split('\n'))
+
+    for s in spells:
+        print('* ' + s.oneline())
+        print(indent(s.text))
