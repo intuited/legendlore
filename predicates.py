04a2e9c307891c90153860f1a498387ecbf6ed48
Ted Tibbetts <ted@circulated.ca>
2021-04-04 17:14:07 -0300

    Added or_ predicate; doctests pass.

--- predicates.py
+++ predicates.py
@@ -30,3 +30,49 @@ def key(val):
 def in_(val):
     """Check if `val` is in the value of `attr`."""
     return lambda attr, obj: hasattr(obj, attr) and val in getattr(obj, attr)
+
+def or_(*preds):
+    """Check if any of the passed predicates return true.
+
+    >>> class TestClass:
+    ...     None
+    >>> def aobj(value, attr='a'):
+    ...     o = TestClass()
+    ...     setattr(o, attr, value)
+    ...     return o
+
+    >>> numor = or_(in_(1), in_(2), in_(3))
+    >>> numor('a', aobj([1, 2, 3]))
+    True
+    >>> numor('a', aobj([4, 5, 6]))
+    False
+    >>> numor('a', aobj([1, 5, 6]))
+    True
+    >>> numor('a', aobj([6, 2, 6]))
+    True
+    >>> numor('a', aobj([8, 7, 1]))
+    True
+    >>> numor('a', aobj([]))
+    False
+    >>> numor('a', aobj([3, 2, 1]))
+    True
+
+    >>> ranges = or_(lt(4), gt(7)) #tests that numbers are outside of the 4-7 range
+    >>> ranges('a', aobj(4))
+    False
+    >>> ranges('a', aobj(3))
+    True
+    >>> ranges('a', aobj(-4.7))
+    True
+    >>> ranges('a', aobj(7))
+    False
+    >>> ranges('a', aobj(8))
+    True
+    """
+    def or_closure(*args, **kwargs):
+        for p in preds:
+            if p(*args, **kwargs):
+                return True
+        return False
+
+    return or_closure
