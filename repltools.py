b644e0b2fb4663c3c99de4adaaf7392bf5b4e331
Ted Tibbetts <ted@circulated.ca>
2021-06-05 05:22:16 -0300

    Further cleanup of repltools
    Wrote module docstring
    Improved organization of `druid` object
    Moved probability stuff into `prob` object.

--- repltools.py
+++ repltools.py
@@ -1,3 +1,15 @@
+"""repltools.py
+
+A set of functions and objects intended to provide convenient access
+to functionality commonly used in a REPL shell like ipython.
+
+Can be sourced into the current REPL environment using ipython's %run command:
+
+    %run -i /path/to/repltools.py
+
+Functionality:
+    #TODO
+"""
 from simpleeval import simple_eval
 import re
 
@@ -19,9 +31,15 @@ asbonus = lambda ascore: int((ascore - 10) / 2)
 ### just druid stuff
 
 druid = Blank()
+druid.__doc__ = """Functions and data structures pertinent to druids."""
 
 def beasts(cr, fly=True, swim=True, crpred=p.lte):
-    """Returns beast-type creatures matching the other conditions."""
+    """Returns beast-type creatures matching the other conditions.
+
+    `crpred` is a predicate factory from `dnd5edb.predicates`.
+    It's passed `cr` and the returned function is used to filter results
+    based on the creature CR.
+    """
     ret = m.where(type='beast', cr=crpred(cr))
     if not fly:
         ret = ret.where(speed=p.not_(p.contains('fly')))
@@ -48,16 +66,22 @@ del elementals
 druid.moonws = { k: v.sorted('cr') for k, v in druid.moonws.items() }
 
 # AC evaluation for druid mc
-druid.bearbarian_ac = lambda c: max(c.ac_num if 'natural armor' in c.ac.lower() else 0, asbonus(10 + c.dex + c.con))
-druid.drunk_ac = lambda c, wis: max(c.ac_num if 'natural armor' in c.ac.lower() else 0, asbonus(10 + c.dex + wis))
+druid.ac = Blank()
+druid.ac.bearbarian = lambda creature: max(creature.ac_num if 'natural armor' in creature.ac.lower() else 0, asbonus(10 + creature.dex + creature.con))
+druid.ac.bearbarian.__doc__ = 'Calculate AC for Druid/Barbarian wildshaped into `creature`.'
+druid.ac.drunk = lambda creature, wis: max(creature.ac_num if 'natural armor' in creature.ac.lower() else 0, asbonus(10 + creature.dex + wis))
+druid.ac.drunk.__doc__ = 'Calculate AC for Druid/Monk with Wisdom `wis` wildshaped into `creature`.'
 
 ### die roll stuff
+
+prob = Blank()
+prob.__doc__ = """Probability functionality."""
 
 # average die roll calculations
 def calc_avg(expression):
     """Calculates the average total of `expression`.
 
-    `expression can contain die-roll notation of the form [0-9]+d[0-9]+.
+    `expression` can contain die-roll notation of the form [0-9]+d[0-9]+.
 
     >>> calc_avg('1')
     1
@@ -76,7 +100,9 @@ def calc_avg(expression):
     d_re = r'\b([0-9]+)d([0-9]+)\b'
     subbed = re.sub(d_re, r'(float(\1)*\2 + \1)/2', expression)
     return simple_eval(subbed)
+prob.avg = calc_avg
+del calc_avg
 
 # die roll odds
-meetorbeat = lambda minimum: 1 - (float(minimum) - 1) / 20
-adv = lambda odds: 1 - ((1 - odds) * (1 - odds))
+prob.meetorbeat = lambda minimum: 1 - (float(minimum) - 1) / 20
+prob.adv = lambda odds: 1 - ((1 - odds) * (1 - odds))
